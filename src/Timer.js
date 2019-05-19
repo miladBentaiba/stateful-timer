c9da4c90f6ef8748ffee9bb039d88a2174de3b62
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-19 01:21:43 +0100

    add state

--- src/Timer.js
+++ src/Timer.js
@@ -1,27 +1,63 @@
 import React from 'react';
 
-export default function Timer({time}) {
-  let hms={
-      h: Math.floor(time/3600000),
-      m: Math.floor((time/1000%3600)/60),
-      s: time/1000%60
-  }
-  return (
-    <div className="parent">
-      <div className="timer">
-        <p className="time h">{hms.h.toString().length>=2?hms.h:("00" + hms.h).slice(-2)}</p>
-        <p className="time">:</p>
-        <p className="time m">{("00" + hms.m).slice(-2)}</p>
-        <p className="time">:</p>
-        <p className="time s">{("00" + hms.s).slice(-2)}</p>
-      </div>
-      <div className="labels">
-        <p className="label">Hour</p>
-        <p className="label"></p>
-        <p className="label">Minute</p>
-        <p className="label"></p>
-        <p className="label">Second</p>
-      </div>
-    </div>
-  );
+export default class Timer extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            h: Math.floor(props.time/3600000),
+            m: Math.floor((props.time/1000%3600)/60),
+            s: Math.round(props.time/1000)%60,
+            inOn: false,
+            start: true
+        };
+    }
+    
+
+    startTimer() {
+        this.setState({
+        isOn: true,
+        time: this.state.time,
+        start: Date.now() - this.state.time
+        })
+        this.timer = setInterval(() => this.setState({
+        time: Date.now() - this.state.start
+        }), 1);
+    }
+
+    stopTimer() {
+        this.setState({isOn: false})
+        clearInterval(this.timer)
+    }
+
+    resetTimer() {
+        this.setState({time: 0, isOn: false})
+    }
+
+    render() {
+        // let start = (this.state.time == 0) ? <button onClick={this.startTimer}>start</button> : null
+        // let stop = (this.state.time == 0 || !this.state.isOn) ? null : <button onClick={this.stopTimer}>stop</button>
+        // let resume = (this.state.time == 0 || this.state.isOn) ? null : <button onClick={this.startTimer}>resume</button>
+        // let reset = (this.state.time == 0 || this.state.isOn) ? null : <button onClick={this.resetTimer}>reset</button>
+        console.log("state", this.state)
+
+        return (
+          <div className="parent">
+            <div className="timer">
+              <p className="time h">{this.state.h.toString().length>=2?this.state.h:("00" + this.state.h).slice(-2)}</p>
+              <p className="time">:</p>
+              <p className="time m">{("00" + this.state.m).slice(-2)}</p>
+              <p className="time">:</p>
+              <p className="time s">{("00" + this.state.s).slice(-2)}</p>
+            </div>
+            <div className="labels">
+              <p className="label">Hour</p>
+              <p className="label"></p>
+              <p className="label">Minute</p>
+              <p className="label"></p>
+              <p className="label">Second</p>
+            </div>
+          </div>
+        );
+      }
 }
+
